5ae146632258be2b84e46e35b7f7dbfed437cf69
kano1201 <kano.nakamura3@gmail.com>
2021-09-23 06:01:36 +0000

    make_dm

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#about'
   get 'top' => 'homes#top'
-  get '/search' => 'search#search'
+  get '/search' => 'search#search' #検索
+
+  get 'chat/:id', to: 'chats#show', as: 'chat' #DM
+  resources :chats, only: [:create]
 
   resources :users, only: [:show, :edit, :update, :index, :unsubscribe, :withdraw] do
     collection do
@@ -10,15 +13,15 @@ Rails.application.routes.draw do
       patch 'user/withdraw' => 'users#withdraw', as: 'withdraw_user'
       put 'user/withdraw' => 'users#withdraw'
     end
-    resource :relationships, only: [:create, :destroy]
+    resource :relationships, only: [:create, :destroy] #Follow
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
     get 'favorites' => 'favorites#favorites', as: 'favorites'
   end
 
   resources :photos, only: [:show, :edit, :update, :index, :destroy, :new, :create] do
-    resource :likes, only: [:create, :destroy]
-    resources :comments, only: [:create, :destroy]
-    resource :favorites, only: [:create, :destroy]
+    resource :likes, only: [:create, :destroy] #いいね
+    resources :comments, only: [:create, :destroy] #コメント
+    resource :favorites, only: [:create, :destroy] #お気に入り
   end
 end
